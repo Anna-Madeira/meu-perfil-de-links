6e387b2367b09f16075f0339ff625fe770d9b905
Anna Madeira <69610009+Anna-Madeira@users.noreply.github.com>
2025-08-01 22:33:56 -0300

    feat: first commit of the professional links page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,66 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
+import React from "react";
+import {Github, Linkedin, Mail, MapPin, ScrollText, Laptop, User} from "lucide-react";
+interface LinkItem {
+  name: string;
+  url: string;
+  icon: React.ElementType; }
 
-export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+const links: LinkItem[] = [
+  { name: 'Github', url: "https://github.com/Anna-Madeira", icon: Github},
+  {name: 'LinkedIn', url: "https://linkedin.com/in/anna-madeira", icon: Linkedin},
+  {name: "Curriculo", url: "https://curriculo-anna.vercel.app/", icon: ScrollText},
+  {name: "Contato", url: "mailto:anna.madeira@icloud.com", icon: Mail },
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+];
+
+export default function App(){
+const [profile] = useState ({
+  name: "Anna Madeira",
+  bio: "Desenvolvedora Frontend Junior",
+  location: "São Paulo, SP"
+});
+
+return(
+<div className="min-h-screen bg-black text-white flex flex-col items-center font-sans">
+  <div className="bg-pink-300 p-8 my-10 mx-auto rounded-3xl shadow-2xl w-full max-w-lg">
+      <header className="text-center mb-10 w-full max-w-md animate-fade-in"> 
+      <div className="flex items-center justify-center space-x-2 text-pink-900 text-3xl font-extrabold font-serif  tracking-tight mb-2">
+          <User className="w-8 h-8 text-black" />
+          <h1>{profile.name}</h1>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+       <div className="flex items-center justify-center space-x-2 text-white font-serif italic font-semibold ">
+          <MapPin className="w-4 h-4 text-black" />
+          <p>{profile.location}</p>
+        </div>
+      <div className="flex items-center justify-center space-x-2 text-white font-serif italic font-semibold mb-1">
+          <Laptop className="w-4 h-4 text-black" />
+          <p>{profile.bio}</p>
+        </div>
+    </header>
+    <main>
+      <ul>
+        {links.map((links, index) => (
+          <li key={index}>
+            <a href={links.url} target="_blank" rel="noopener noreferrer" className=" flex items-center justify-between transition-all
+            duration-300 py-4 px-6 rounded-xl text-lg font-semibold text-pink-900 shadow-md hover:bg-pink-200
+            transform hover:-translate-y-1 border border-slate-700"> 
+            <div className="flex items-center space-x-4">
+              <links.icon className="w-6 h-6 text-black"/>
+              <span>{links.name}</span>
+            </div>
+            </a>
+
+          </li>
+        ))}
+      </ul>
+    </main>
+    </div>
+    <footer className="mt-10 text-center text-white text-sm">
+        <p>Desenvolvido com React.js, Typescript e Tailwind CSS</p>
       </footer>
     </div>
-  );
+  
+);
 }
